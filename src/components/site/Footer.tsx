f692764f4348152c2064eca4e41bb254cc5028e5
Khushboo Magsi <khushboomagsi39@gmail.com>
2026-07-09 19:07:28 +0500

    Updated homepage text

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
           </div> */}
           <div>
   <img
-    src="/Imges/76.png"
+    src="/Imges/90.jpeg"
     alt="The M.T School System"
     className="h-16 w-auto"
   />
